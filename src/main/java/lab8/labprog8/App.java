1a71ac1cf40ae3fdb0492165e22d905501be5e1f
Adibhaktha07 <adibhaktha07@gmail.com>
2025-06-05 06:03:51 +0530

    first commit

---
+++ src/main/java/lab8/labprog8/App.java
@@ -0,0 +1,13 @@
+package lab8.labprog8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
